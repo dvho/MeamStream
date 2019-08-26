9f26d1dc1c6726ee89eee9178c0a30b2431c97e7
David <allhits@gmail.com>
2019-08-26 01:20:27 -0400

    Prevent users from sending messages to themselves

--- src/components/views/SendMessage.js
+++ src/components/views/SendMessage.js
@@ -12,7 +12,7 @@ import actions from '../../redux/actions'
 
 //To hide toolbar TouchableOpacities upon clicking the T icon to toggle to regular text message (when all of the Png layers and canvas are empty strings) it would've been ideal to make visiblity 'hidden' when this.state.selectedLayer === 'message' but there's a known bug in React Native so I made opacities 0 AND rendered the onPress functions null https://github.com/facebook/react-native/issues/1322
 
-//TODO: 6) Prevent users from sending themselves messages, 7) Make logout unavailable until screen finishes loading, make tab navs inactive until screens finish loading, 11) need to add Powered By Giphy and appy for a development API key and then production API key https://developers.giphy.com/faq/, 12) Need to move API from staging site to production (new acct)
+//TODO: 7) Make logout unavailable until screen finishes loading, make tab navs inactive until screens finish loading, 11) need to add Powered By Giphy and appy for a development API key and then production API key https://developers.giphy.com/faq/, 12) Need to move API from staging site to production (new acct)
 
 
 class SendMessage extends React.Component {
@@ -210,6 +210,11 @@ class SendMessage extends React.Component {
 
         send() {
             let params = this.state.newMessage
+            if (this.props.state.account.user.username === params.toUser) {
+                alert('You can\'t send a message to yourself lol!')
+                this.updateNewMessage('', 'toUser')
+                return
+            }
             utils
             .createMessages(params)
             .then(data => {
